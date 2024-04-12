77b4f65fd3e2b036142ec889c93b2abb38fc7d66
Ricky Tang <rickytang2019@gmail.com>
2024-04-13 00:29:35 +0100

    Added footer
    Known issue: weird indent in src because of weird formatting plugin used

---
+++ webcomponents/FooterComponent.js
@@ -0,0 +1,63 @@
+class CustomFooter extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: "open"
+        });
+        this.loadStyles();
+    }
+
+    connectedCallback() {
+        this.render();
+        // this.attachInfo();
+    }
+
+    loadStyles() {
+        const styles = document.createElement("style");
+        styles.textContent = `
+:host {
+position: absolute;
+bottom: 0;
+width: 100%;
+font-family: var(--font-main, sans-serif);
+border: 1px solid;
+}
+
+.footer {
+
+}
+
+ul {
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+gap: 10px;
+margin: 0;
+padding: 10px;
+list-style: none;
+}
+`;
+        this.shadowRoot.appendChild(styles);
+    }
+
+    render() {
+        // Create and append the footer template
+        const template = document.createElement("template");
+        template.innerHTML = /*html*/ `
+<footer class="footer">
+    <ul>
+        <li><a href="https://github.com/neverIand">Github</a></li>
+        <li><a href="mailto:rickytang2019@gmail.com">Email</a></li>
+        <li><a href="https://www.youtube.com/@ratch3t673">Youtube</a></li>
+    </ul>
+</footer>
+`;
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+if (!customElements.get("berry-footer")) {
+    customElements.define("berry-footer", CustomFooter);
+}
+
+export default CustomFooter;
--- webcomponents/LogoComponent.js
+++ webcomponents/LogoComponent.js
@@ -1,119 +1,120 @@
 class LogoComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.render();
-  }
-
-  static get observedAttributes() {
-    return ["size"];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (name === "size") {
-      this.updateStyles();
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: "open"
+        });
+        this.render();
     }
-  }
 
-  render() {
-    const style = document.createElement("style");
-    this.shadowRoot.appendChild(style);
+    static get observedAttributes() {
+        return ["size"];
+    }
 
-    const wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "square outline");
-    wrapper.innerHTML = `
-        <div class="square left-square"></div>
-        <div class="square right-square"></div>
-        <div class="square core"></div>
-      `;
-    this.shadowRoot.appendChild(wrapper);
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "size") {
+            this.updateStyles();
+        }
+    }
 
-    this.updateStyles();
-  }
+    render() {
+        const style = document.createElement("style");
+        this.shadowRoot.appendChild(style);
 
-  updateStyles() {
-    const size = this.getAttribute("size") || "240"; // Default size
-    const isAnimated = this.getAttribute("animate") || "true";
-    const length = parseInt(size);
-    const borderWidth = length * 0.04;
-    const borderRadius = length * (2 / 75);
-    const leftSquareWidth = length * (2 / 3);
-    const rightSquareWidth = length / 2;
-    const coreWidth = length / 3;
+        const wrapper = document.createElement("div");
+        wrapper.setAttribute("class", "square outline");
+        wrapper.innerHTML = /*html*/ `
+<div class="square left-square"></div>
+<div class="square right-square"></div>
+<div class="square core"></div>
+`;
+        this.shadowRoot.appendChild(wrapper);
+        this.updateStyles();
+    }
 
-    const styles = `
-        :host {
-          --length: ${length}px;
-          --logo-border-width: ${borderWidth}px;
-          --logo-border-radius: ${borderRadius}px;
-          --logo-left-square-width: ${leftSquareWidth}px;
-          --logo-right-square-width: ${rightSquareWidth}px;
-          --logo-core-width: ${coreWidth}px;
-        }
-  
-        .square {
-          background-color: white;
-          border: var(--logo-border-width) solid purple;
-          border-radius: var(--logo-border-radius);
-        }
-  
-        .outline {
-          position: relative;
-          width: var(--length);
-          height: var(--length);
-          z-index: 200;
-          overflow: hidden;
-        }
-  
-        .left-square {
-          position: absolute;
-          top: calc(var(--length) * 0.3);
-          left: calc(0px - var(--length) / 3);
-          width: var(--logo-left-square-width);
-          height: var(--logo-left-square-width);
-          z-index: 100;
-          ${isAnimated === "true" ? "" : "transform: rotateZ(60deg);"}
-          animation: ${
-            isAnimated === "true"
-              ? "transformLeft 4s infinite linear"
-              : "inherit"
-          };
-        }
-  
-        .right-square {
-          position: absolute;
-          top: calc(var(--length) * calc(1 / 15));
-          left: calc(var(--length) * calc(11 / 15));
-          transform: rotateZ(60deg);
-          width: var(--logo-right-square-width);
-          height: var(--logo-right-square-width);
-          z-index: 50;
-          animation: ${
-            isAnimated === "true" ? "transformLeft 4s infinite linear" : "none"
-          };
-        }
-  
-        .core {
-          position: relative;
-          top: calc(var(--logo-core-width) - var(--logo-border-width) / 2);
-          width: var(--logo-core-width);
-          height: var(--logo-core-width);
-          margin: auto;
-          border: var(--logo-border-width) solid black;
-        }
-  
-        @keyframes transformLeft {
-          from {
-            transform: translateX(-440px) rotateZ(60deg);
-          }
-          to {
-            transform: translateX(520px) rotateZ(-300deg);
-          }
-        }
-      `;
+    updateStyles() {
+        const size = this.getAttribute("size") || "240"; // Default size
+        const isAnimated = this.getAttribute("animate") || "true";
+        const length = parseInt(size);
+        const borderWidth = length * 0.04;
+        const borderRadius = length * (2 / 75);
+        const leftSquareWidth = length * (2 / 3);
+        const rightSquareWidth = length / 2;
+        const coreWidth = length / 3;
 
-    this.shadowRoot.querySelector("style").textContent = styles;
-  }
+        const styles = `
+:host {
+--length: ${length}px;
+--logo-border-width: ${borderWidth}px;
+--logo-border-radius: ${borderRadius}px;
+--logo-left-square-width: ${leftSquareWidth}px;
+--logo-right-square-width: ${rightSquareWidth}px;
+--logo-core-width: ${coreWidth}px;
+}
+
+.square {
+background-color: white;
+border: var(--logo-border-width) solid purple;
+border-radius: var(--logo-border-radius);
+}
+
+.outline {
+position: relative;
+width: var(--length);
+height: var(--length);
+z-index: 200;
+overflow: hidden;
+}
+
+.left-square {
+position: absolute;
+top: calc(var(--length) * 0.3);
+left: calc(0px - var(--length) / 3);
+width: var(--logo-left-square-width);
+height: var(--logo-left-square-width);
+z-index: 100;
+${isAnimated === "true" ? "" : "transform: rotateZ(60deg);"}
+animation: ${
+isAnimated === "true"
+? "transformLeft 4s infinite linear"
+: "inherit"
+};
+}
+
+.right-square {
+position: absolute;
+top: calc(var(--length) * calc(1 / 15));
+left: calc(var(--length) * calc(11 / 15));
+transform: rotateZ(60deg);
+width: var(--logo-right-square-width);
+height: var(--logo-right-square-width);
+z-index: 50;
+animation: ${
+isAnimated === "true" ? "transformLeft 4s infinite linear" : "none"
+};
+}
+
+.core {
+position: relative;
+top: calc(var(--logo-core-width) - var(--logo-border-width) / 2);
+width: var(--logo-core-width);
+height: var(--logo-core-width);
+margin: auto;
+border: var(--logo-border-width) solid black;
+}
+
+@keyframes transformLeft {
+from {
+transform: translateX(-440px) rotateZ(60deg);
+}
+to {
+transform: translateX(520px) rotateZ(-300deg);
+}
+}
+`;
+
+        this.shadowRoot.querySelector("style").textContent = styles;
+    }
 }
 
 customElements.define("berry-logo", LogoComponent);
