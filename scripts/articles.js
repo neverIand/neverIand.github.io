5137137522dd5e1e018597ebfb3dd80c3fcd4d11
Ricky Tang <rickytang2019@gmail.com>
2024-05-03 19:38:46 +0100

    article responsiveness fix

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,5 +1,5 @@
 import "/webcomponents/DateComponent/DateComponent.js";
 import "/webcomponents/SubheadingComponent/SubheadingComponent.js";
-import '/webcomponents/CodeSnippetComponent/CodeSnippetComponent.js'
+import "/webcomponents/CodeSnippetComponent/CodeSnippetComponent.js";
 
 // TODO? Change the browser's tab title
