bf3b0441117da3125ea39b3a3b003d97c8added3
Ricky Tang <rickytang2019@gmail.com>
2024-09-26 20:39:14 +0100

    added `<berry-chip>`

--- webcomponents/ChipComponent/ChipComponent.js
+++ webcomponents/ChipComponent/ChipComponent.js
@@ -12,13 +12,30 @@ class ChipComponent extends HTMLElement {
     this.render();
   }
 
+  // TODO? clickable option
   render() {
+    const label = this.getAttribute("data-label") || "";
     const template = document.createElement("template");
     template.innerHTML = /*html*/ `
-    <div>
-    chip
-    </div>
+    <span title="${label}">${label}</span>
 `;
+    // TODO: hover state
+    this.styles = document.createElement("style");
+    this.styles.innerHTML = `
+    :host{
+      display: inline-block;
+    }
+    span{
+      margin: 0 4px;
+      padding: 0 8px;
+      color: white;
+      background-color: rgb(100, 100, 100);
+      border-radius: 4px;
+      cursor: default;
+    }
+    `;
+
+    this.shadowRoot.appendChild(this.styles.cloneNode(true));
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
