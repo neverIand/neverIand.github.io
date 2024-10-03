a77943203697ee15d5386c26befe8aa23ba24f34
neverIand <neverIand@users.noreply.github.com>
2024-10-03 17:13:07 +0000

    deploy: 91cb29c0ee8efe7d232ab407106d73e3b68b7184

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,5 +1,1 @@
-import "/webcomponents/DateComponent/DateComponent.js";
-import "/webcomponents/SubheadingComponent/SubheadingComponent.js";
-import "/webcomponents/CodeSnippetComponent/CodeSnippetComponent.js";
-import '/webcomponents/ToastComponent/ToastComponent.js'
-// TODO? Change the browser's tab title
+import"/webcomponents/DateComponent/DateComponent.js";import"/webcomponents/SubheadingComponent/SubheadingComponent.js";import"/webcomponents/CodeSnippetComponent/CodeSnippetComponent.js";import"/webcomponents/ToastComponent/ToastComponent.js";
