262de570aada4cdabf1a3d24e66e2815bc6670b6
neverIand <neverIand@users.noreply.github.com>
2024-12-30 22:18:57 +0000

    deploy: aee376807790281acdce8fcfbd5ddb941d777e7c

---
+++ scripts/preloadStyles.js
@@ -0,0 +1,1 @@
+export function preloadStyles(e){const t=document.createElement("link");t.setAttribute("rel","preload"),t.setAttribute("as","style"),t.setAttribute("href",e),document.head.appendChild(t)}
